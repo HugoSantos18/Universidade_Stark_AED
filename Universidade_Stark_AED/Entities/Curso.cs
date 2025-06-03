d3a9d9a416105c78ddd71c6c02c83da1acd2eba4
lcs_almeida_s <lcshenriquegs@gmail.com>
2025-06-03 14:49:17 -0300

    Update classe Curso - Criação da Lista e FilaFlexivel

--- Universidade_Stark_AED/Entities/Curso.cs
+++ Universidade_Stark_AED/Entities/Curso.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Universidade_Stark_AED.DataStructures;
 
 namespace Universidade_Stark_AED.Entities
 {
@@ -12,12 +13,17 @@ namespace Universidade_Stark_AED.Entities
         private string _nomeCurso;
         private int _maxVagas;
 
+        private List<Candidato> _candidatosSelecionados;
+        private FilaFlexivel<Candidato> _filaEspera;
 
         public Curso(int codigoCurso, string nomeCurso, int maxVagas)
         {
             this._codigoCurso = codigoCurso;
             this._nomeCurso = nomeCurso;
             this._maxVagas = maxVagas;
+            List<Candidato> _candidatosSelecionados = new List<Candidato>();
+            FilaFlexivel<Candidato> _filaEspera = new FilaFlexivel<Candidato>();
+
         }
 
         public int CodigoCurso { get; set; }
@@ -25,3 +31,4 @@ namespace Universidade_Stark_AED.Entities
         public int MaxVagas { get; set; }
     }
 }
+
