7385cbff6f4de994c1e50177be6b562d841eb8b1
lcs_almeida_s <lcshenriquegs@gmail.com>
2025-06-03 14:34:43 -0300

    Update Classe Celula e FilaFlexivel.  Ambas Genéricas

--- Universidade_Stark_AED/DataStructures/Celula.cs
+++ Universidade_Stark_AED/DataStructures/Celula.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace Universidade_Stark_AED.DataStructures
 {
-    class Celula
+    class Celula<T>
     {
-        // TO DO
+        private T elemento;
+        private Celula<T> prox;
+
+        public Celula(T elemento)
+        {
+            this.elemento = elemento;
+            this.prox = null;
+        }
+
+        public Celula()
+        {
+            this.elemento = default(T);
+            this.prox = null;
+        }
+
+        public Celula<T> Prox
+        {
+            get { return prox; }
+            set { prox = value; }
+        }
+        public T Elemento
+        {
+            get { return elemento; }
+            set { elemento = value; }
+        }
     }
 }
+
--- Universidade_Stark_AED/DataStructures/FilaFlexivel.cs
+++ Universidade_Stark_AED/DataStructures/FilaFlexivel.cs
@@ -6,8 +6,52 @@ using System.Threading.Tasks;
 
 namespace Universidade_Stark_AED.DataStructures
 {
-    class FilaFlexivel
+    class FilaFlexivel<T>
     {
-        // TO DO
+        private Celula<T> primeiro, ultimo;
+
+        public FilaFlexivel()
+        {
+            primeiro = ultimo = new Celula<T>(default);
+        }
+
+        public void Inserir(T elemento)
+        {
+            ultimo.Prox = new Celula<T>(elemento);
+            ultimo = ultimo.Prox;
+        }
+
+        public T Remover()
+        {
+            if (primeiro == ultimo)
+                throw new InvalidOperationException("Fila vazia!");
+
+            Celula<T> tmp = primeiro.Prox;
+            primeiro = primeiro.Prox;
+
+            T removido = primeiro.Elemento;
+            tmp.Prox = null;
+
+            return removido;
+        }
+
+        public bool Pesquisar(T elemento)
+        {
+            for (Celula<T> i = primeiro.Prox; i != null; i = i.Prox)
+            {
+                if (i.Elemento.Equals(elemento))
+                    return true;
+            }
+            return false;
+        }
+
+        public void Exibir()
+        {
+            for (Celula<T> i = primeiro.Prox; i != null; i = i.Prox)
+            {
+                Console.WriteLine(i.Elemento);
+            }
+        }
     }
+
 }
