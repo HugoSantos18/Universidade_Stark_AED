2e426f6c3895acc0a4e1cd1cee4f558d450ed5e0
Hugo Silva <Hugo0312@outlook.com>
2025-06-04 02:50:09 -0300

    Finalização da classe para leitura de arquivos e adiciona-los a uma estrutura de dados para armazenamento, implementação de métodos da classe Curso e Candidato e começo da implementação da classe processoSeletivo.

--- Universidade_Stark_AED/Entities/Candidato.cs
+++ Universidade_Stark_AED/Entities/Candidato.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,28 +12,55 @@ namespace Universidade_Stark_AED.Entities
         private string _nomeCandidato;
         private double _notaRedacao;
         private double _notaMatematica;
-        private double _notaPortugues;
+        private double _notaLinguagens;
         private int _codigoPrimeiraOpcao;
         private int _codigoSegundaOpcao;
+        private const int NumeroProvas = 3;
 
-
-        public Candidato(string nomeCandidato, double notaRedacao, double notaMatematica, double notaPortugues,
+        public Candidato(string nomeCandidato, double notaRedacao, double notaMatematica, double notaLinguagens,
                                                                    int codigoPrimeiraOpcao, int codigoSegundaOpcao)
         {
             this._nomeCandidato = nomeCandidato;
             this._notaRedacao = notaRedacao;
             this._notaMatematica = notaMatematica;
-            this._notaPortugues = notaPortugues;
+            this._notaLinguagens = notaLinguagens;
             this._codigoPrimeiraOpcao = codigoPrimeiraOpcao;
             this._codigoSegundaOpcao = codigoSegundaOpcao;
         }
 
+        public string GetNomeCandidato()
+        {
+            return _nomeCandidato;
+        }
+        public double GetNotaRedacao()
+        {
+            return _notaRedacao;
+        }
+        public double GetNotaMatematica()
+        {
+            return _notaMatematica;
+        }
+        public double GetNotaLinguagens()
+        {
+            return _notaLinguagens;
+        }
+        public int GetCodigoPrimeiraOpcao()
+        {
+            return _codigoPrimeiraOpcao;
+        }
+        public int GetCodigoSegundaOpcao()
+        {
+            return _codigoSegundaOpcao;
+        }
 
-        public string NomeCandidato { get; set; }
-        public double NotaRedacao { get; set; }
-        public double NotaMatematica { get; set; }
-        public double NotaPortugues { get; set; }
-        public int CodigoPrimeiraOpcao { get; set; }
-        public int CodigoSegundaOpcao { get; set; }
+        public double MediaNotas()
+        {
+            return (_notaRedacao + _notaMatematica + _notaLinguagens) / 2;
+        }
+
+        public override string ToString()
+        {
+            return $"{_nomeCandidato} {MediaNotas()}";
+        }
     }
 }
